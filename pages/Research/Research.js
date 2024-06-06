e717dd8c845da605e87b03cc95df1659e4bc7397
Daniel W. Hieber <dwhieb@gmail.com>
2024-06-06 08:29:37 -0500

    NEW: `<pre>` + `<code>` styles

--- pages/Research/Research.js
+++ pages/Research/Research.js
@@ -1,8 +1,8 @@
 export function Research(req, res) {
 
-  const { pub } = req.params
+  const { publication } = req.params
 
-  res.render(`Research/${ pub }.md`, {
+  res.render(`Research/${ publication }.md`, {
     Research: true,
     title:    `Fedorchak et al. 2023. Strategies for lexical expansion in Algonquian languages.`,
   })
