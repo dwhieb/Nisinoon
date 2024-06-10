74400dc83461b51b52d29568fa250f26a4b3af32
Daniel W. Hieber <dwhieb@gmail.com>
2024-06-10 14:12:10 -0500

    DATA: Convert Base Category

--- data/Components.js
+++ data/Components.js
@@ -44,6 +44,39 @@ function cleanUR(UR) {
   .replace(/\/$/v, ``)
 }
 
+function groupColumns(record, ...colTypes) {
+
+  const keys  = Object.keys(record)
+  const items = []
+
+  for (const colType of colTypes) {
+
+    const letteredColumns = keys.filter(key => key.startsWith(colType))
+
+    for (const letteredColumn of letteredColumns) {
+
+      const [, letter] = letteredColumn.split(`-`)
+      const i          = letterToIndex(letter)
+      const data       = record[letteredColumn]
+
+      if (data) {
+        items[i]    ??= {}
+        const item    = items.at(i)
+        item[colType] = data
+      }
+
+    }
+
+  }
+
+  return items
+
+}
+
+function letterToIndex(letter) {
+  return letter.toLowerCase().charCodeAt(0) - 97
+}
+
 export default class Components extends Map {
 
   static columns = {
@@ -226,6 +259,13 @@ export default class Components extends Map {
       component.primary   = finalType === `B` || !finalType
       component.secondary = finalType === `B` || finalType === `Y`
 
+    }
+
+
+    // Base Category
+    if (component.secondary) {
+      component.baseCategories = groupColumns(record, cols.baseCategory)
+      .map(({ [cols.baseCategory]: baseCategory }) => baseCategory)
     }
 
     // Matches
