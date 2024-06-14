a033e90e6580e2b3361b51f5d72ff51c20da842f
Daniel W. Hieber <dwhieb@gmail.com>
2024-06-14 00:42:23 -0500

    NEW: First + Last Page Links

--- pages/Search/Search.js
+++ pages/Search/Search.js
@@ -1,3 +1,15 @@
+/**
+ *
+ * @param {URL}    url A URL object.
+ * @param {String} param The URL parameter to update.
+ * @param {any}    val The value to update the parameter with.
+ */
+function changeParam(url, param, val) {
+  url = new URL(url.href)
+  url.searchParams.set(param, val)
+  return url.toString()
+}
+
 export function Search(req, res) {
 
   const { db } = req.app
@@ -25,18 +37,23 @@ export function Search(req, res) {
   offset = Number(offset)
   q      = q.trim().toLowerCase()
 
-  let   components   = req.app.db.search(q)
-  const totalResults = components.length.toLocaleString()
+  const allResults = req.app.db.search(q)
+  const results    = allResults.slice(offset, offset + limit)
+  const url        = new URL(req.originalUrl, `${ req.protocol }://${ req.host }`)
 
-  components = components.slice(offset, offset + limit)
-
-  const resultsShown = components.length.toLocaleString()
+  const lastPageOffset = Math.floor(allResults.length / limit) * limit
 
   Object.assign(context, {
-    components,
+    endIndex:   Math.min(offset + limit, allResults.length).toLocaleString(),
     hasResults: true,
-    resultsShown,
-    totalResults,
+    links:      {
+      firstPage: changeParam(url, `offset`, 0),
+      lastPage:  changeParam(url, `offset`, lastPageOffset),
+    },
+    numResults:   results.length.toLocaleString(),
+    results,
+    startIndex:   offset.toLocaleString(),
+    totalResults: allResults.length.toLocaleString(),
   })
 
   res.render(`Search/Search`, context)
