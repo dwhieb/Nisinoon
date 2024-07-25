fcbd2d00b7436360daf4b7000f32a38889c3835c
dwhieb <dwhieb@users.noreply.github.com>
2024-07-25 18:48:47 +0000

    deploy: d4b4f66d32255756f5a2bae3a236c503c89fad05

--- utilities/retryRequest.js
+++ utilities/retryRequest.js
@@ -5,22 +5,28 @@ export default function retryRequest(requestFunction, ...args) {
   let waitTime = 1
 
   const request = async () => {
+
+    let ret
+
     try {
 
-      return requestFunction(...args)
+      ret = await requestFunction(...args)
 
     } catch (e) {
 
-      if (e.response?.status == 429) {
+      if (e.response.status == 429) {
         waitTime *= 2
         console.warn(`\nHit rate limit. Retrying after ${ waitTime }ms.`)
         await setTimeout(waitTime)
-        return request()
+        ret = await request()
+      } else {
+        throw e
       }
 
-      throw e
-
     }
+
+    return ret
+
   }
 
   return request()
