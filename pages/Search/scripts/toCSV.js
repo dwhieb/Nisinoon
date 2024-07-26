6b7ac84ca643276376118a8985c51e889f165678
Daniel W. Hieber <dwhieb@gmail.com>
2024-07-26 13:53:26 -0500

    NEW: Add components to CSV export (#131)
    partially addresses #131

--- pages/Search/scripts/toCSV.js
+++ pages/Search/scripts/toCSV.js
@@ -1,5 +1,9 @@
 import parser from 'papaparse'
 
+function mergeFields(tokens, field) {
+  return tokens.map(token => token[field] || `—`).join(`; `)
+}
+
 function preprocessComponent(data) {
 
   // WARNING: Do not alter the original component
@@ -7,12 +11,29 @@ function preprocessComponent(data) {
 
   // Component Fields
 
-  component.baseCategories = component.baseCategories.join(`; `)
+  component.baseCategories = component.baseCategories?.join(`; `)
   component.matchAI        = component.matches.AI
   component.matchII        = component.matches.II
   component.matchTA        = component.matches.TA
   component.matchTI        = component.matches.TI
-  component.tags           = component.tags.join(`; `)
+  component.tags           = component.tags?.join(`; `)
+
+  // Token Fields
+
+  if (component.tokens) {
+
+    const { tokens } = component
+
+    component[`source.forms`]        = mergeFields(tokens, `form`)
+    component[`source.UR`]           = mergeFields(tokens, `UR`)
+    component[`source.PA`]           = mergeFields(tokens, `PA`)
+    component[`source.orthography`]  = mergeFields(tokens, `orthography`)
+    component[`source.glosses`]      = mergeFields(tokens, `gloss`)
+    component[`source.notes`]        = mergeFields(tokens, `notes`)
+    component[`source.bibliography`] = mergeFields(tokens, `bibliography`)
+    component[`source.speaker`]      = mergeFields(tokens, `speaker`)
+
+  }
 
   // Array Fields
 
