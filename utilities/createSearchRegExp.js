1b66109d4d06659e88566b998dedf71a84e94d53
dwhieb <dwhieb@users.noreply.github.com>
2024-07-27 22:26:26 +0000

    deploy: efd6d6b513387c7a62b9fab9c4422e5d0fb5a8a6

---
+++ utilities/createSearchRegExp.js
@@ -0,0 +1,19 @@
+import escapeRegExp from 'escape-string-regexp'
+
+/**
+ *
+ * @param {String}  query                         The search string.
+ * @param {Object}  [options={}]                  An options object.
+ * @param {Boolean} [options.caseSensitive=false] Whether the search regex should be case sensitive.
+ * @param {Boolean} [options.regex=false]         Whether the search string is a regular expression.
+ * @returns
+ */
+export default function createSearchRegExp(query, { caseSensitive, regex } = {}) {
+
+  const pattern = regex ? query : escapeRegExp(query)
+  const flags   = caseSensitive ? `v` : `iv`
+  const regexp  = new RegExp(pattern, flags)
+
+  return str => regexp.test(str)
+
+}
