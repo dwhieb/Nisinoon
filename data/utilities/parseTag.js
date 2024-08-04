68fe6c56bb2f0c7ab839ae272d92bbfc30aa2e06
Daniel W. Hieber <dwhieb@gmail.com>
2024-08-04 18:33:42 -0500

    DATA: Use lowercase to store tags
    [skip ci]

--- data/utilities/parseTag.js
+++ data/utilities/parseTag.js
@@ -1,8 +1,13 @@
 const grammaticalRegExp = /^([A-Z+\-\(\)]|\s)+$/v
 
-export default function parseTag(tag) {
+export default function parseTag(rawTag) {
+
+  const grammatical = grammaticalRegExp.test(rawTag)
+  const tag         = grammatical ? rawTag.toLowerCase() : rawTag
+
   return {
-    grammatical: grammaticalRegExp.test(tag),
+    grammatical,
     tag,
   }
+
 }
